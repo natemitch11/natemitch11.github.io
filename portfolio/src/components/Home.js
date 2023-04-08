b2d5fa3378f48a7a687332f53157a8fd7002ef30
natemitch11 <nate.mitch11@gmail.com>
2023-04-08 18:12:42 -0500

    remove unused information

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -2,35 +2,6 @@ import { Box, Container, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
-  function addHidden(form) {
-    form.classList.add("hidden");
-  }
-  function removeHidden(form) {
-    form.classList.remove("hidden");
-  }
-  //Get Elements and store them in variables
-  //contact
-  let contactHide = document.getElementById("contact-hide");
-  let closedMail = document.getElementById("closed-env");
-  let openMail = document.getElementById("open-env");
-
-  //event listeners
-  const hideContact = () => {
-    setTimeout(() => {
-      addHidden(openMail);
-      removeHidden(closedMail);
-    }, 300);
-  };
-  const hideMail = () => {
-    addHidden(closedMail);
-    removeHidden(openMail);
-    setTimeout(() => {
-      alert(
-        "Sorry, I hid it up here because I don't like spam... Feel free to copy my email though! (its also on my resume...) nate.mitch11@gmail.com"
-      );
-    }, 300);
-  };
-
   return (
     <Container>
       <Box
